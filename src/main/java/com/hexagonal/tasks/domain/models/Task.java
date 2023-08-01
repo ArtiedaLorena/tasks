a25570a5ae00e49f1e84f77945224971ee7e1d57
LOrena Artieda <artiedalorena@gmail.com>
2023-08-01 17:48:07 -0300

    Capa de dominio completa

---
+++ src/main/java/com/hexagonal/tasks/domain/models/Task.java
@@ -0,0 +1,20 @@
+package com.hexagonal.tasks.domain.models;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime creationDate;
+    private boolean completed;
+
+
+    public Task(Long id, String title, String description, LocalDateTime creationDate, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.creationDate = creationDate;
+        this.completed = completed;
+    }
+}
